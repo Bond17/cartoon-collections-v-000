ebc04b7d17520002ff56d353027ee6901be65354
Robert Watson <spartan83@gmail.com>
2017-12-10 19:09:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet (array)# code an argument here
   # Your code here
   summon = []
-  summon = array.collect do [element]
+  summon = array.collect do |element|
     element.capitalize + "!"
   end
   summon
